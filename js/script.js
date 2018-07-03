4fd4e30f1a835039fb8e3dfe6b686ead8d402dc1
PapkaRimskyi <magnezia98@mail.ru>
2018-07-03 16:12:41 +0300

    Завершение

--- js/script.js
+++ js/script.js
@@ -2,21 +2,55 @@ var buttonOpen = document.querySelector(".open-popup");
 var popup = document.querySelector(".modal-write-us");
 var buttonClose = document.querySelector(".button-close");
 var form = popup.querySelector("form");
-var login = popup.querySelector([name="login"]);
-var email = popup.querySelector([name="comment"]);
+var login = popup.querySelector("[name=login]");
+var email = popup.querySelector("[name=email]");
+var storage = localStorage.getItem("login");
+var isStorageSupport = true;
+var storage = "";
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+      evt.preventDefault();
+    }
+  }
+});
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 buttonOpen.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
+  if (storage) {
+    login.value = storage;
+    email.focus();
+  } else {
+    login.focus();
+  }
 });
 
 buttonClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function(evt) {
-  evt.preventDefault();
-  console.log(login.value);
-  console.log(email.value);
+  if (!login.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+    console.log("Заполните пустые поля");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+    }
+  }
 });
